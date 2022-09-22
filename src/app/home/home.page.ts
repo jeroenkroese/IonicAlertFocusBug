c8689f32d2fcf41f3a1cea06c7ec0bad9682968a
Jeroen Kroese <jeroen@picca.app>
2022-09-22 19:26:25 +0200

    Initial commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActionSheetController, AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,31 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private alertController: AlertController, private actionSheetController: ActionSheetController) {}
 
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Alert',
+      subHeader: 'Subtitle',
+      message: 'This is an alert message.',
+      buttons: ['Cancel', 'OK']
+    });
+      await alert.present();
+  }
+
+
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Albums',
+      buttons: [ {
+        text: 'Open alert',
+        icon: 'share',
+        handler: () => {
+          this.presentAlert();
+        }
+      }]
+    });
+
+    await actionSheet.present();
+  }
 }
